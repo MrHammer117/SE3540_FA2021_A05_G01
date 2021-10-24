bb63cdcc5a41b73b2ae42e42ee8d1d50108e1442
HP <lucasjh@bgsu.edu>
2021-10-24 18:55:23 -0400

    yahtzee

---
+++ Yahtzee2.cpp
@@ -0,0 +1,40 @@
+// Yahtzee2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+class Game {
+public:
+    void play();
+    int totalresult;
+};
+
+ void Game ::play()
+    {
+        string word;
+        cout << "Hello. Welcome to Yahtzee. Type 'play' to play, type 'stop' in order to exit the game." << endl;
+        cin >> word;
+        if (word == "play") {
+            cout << "Rolling Dice..." << endl;
+            srand(time(0));
+            Dice object1;
+            Show scorecard;
+            choose cat
+                show updated scoreboard
+                roll dice again.
+
+
+        };
+
+        if (word == "stop") {
+            cout << "Leaving game.";
+
+        };
+        if (word != "stop" && word != "play") {
+            cout << "Error! Please try again.";
+        };
+    };
+
+}
+
+
