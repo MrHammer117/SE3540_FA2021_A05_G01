75a18a2fcfea233489457eabef2df68a153897a4
Lucas Hufford <lucasjh@bgsu.edu>
2021-10-28 20:28:33 +0000

    Updated menu options and error messages

--- Yahtzee2.cpp
+++ Yahtzee2.cpp
@@ -13,8 +13,12 @@ public:
  void Game ::play()
     {
         string word;
-        cout << "Hello. Welcome to Yahtzee. Type 'play' to play, type 'stop' in order to exit the game." << endl;
+        string word2;
+    cout << "Hello. Welcome to Yahtzee." << endl;
+     cout << "Type 'play' to play Yahtzee." << endl;
+     cout << "Type 'stop' if you do not want to play the game." << endl;
         cin >> word;
+        
         if (word == "play") {
             cout << "Rolling Dice..." << endl;
             srand(time(0));
@@ -23,16 +27,60 @@ public:
             choose cat
                 show updated scoreboard
                 roll dice again.
-
-
-        };
+};
 
         if (word == "stop") {
-            cout << "Leaving game.";
+         cout << "Leaving game.";
+     };
+     if (word != "stop" && word != "play") {
+         cout << "Error! Please try again.";
+         cout << "Hello. Welcome to Yahtzee." << endl;
+         cout << "Type 'play' to play Yahtzee." << endl;
+         cout << "Type 'stop' if you do not want to play the game." << endl;
+         cin >> word;
+     };
+         cout << "Would you like to roll the dice? Enter 'yes' to roll the dice. Enter 'no' to not roll the dice. Enter 'stop' to stop the game." << endl;
+         cin >> word2;
+         if (word2 == "no") {
+             cout << "Would you like to roll the dice? Enter 'yes' to roll the dice. Enter 'no' to not roll the dice." << endl;
+             cin >> word2;
+         };
+         if (word2 == "exit") {
+             cout << "Closing game. Goodbye";
+         }
+         if (word2 != "no" && word2 != "yes" && word2 != "stop") {
+             cout << "Input not valid. Please try again";
+             cout << "Would you like to roll the dice? Enter 'yes' to roll the dice. Enter 'no' to not roll the dice." << endl;
+             cin >> word2;
+         };
 
-        };
-        if (word != "stop" && word != "play") {
-            cout << "Error! Please try again.";
+         if (word2 == "yes") {
+             cout << "Rolling Dice..." << endl;
+             Dice object;
+             scoreboard object2;
+             object2.DisplayScoringRules;
+             for (int i = 0; i <= 13; i++) {
+             srand(time(0));
+             object.rollDice;
+             object.sortDice;
+             object2.DisplayCat;
+             object2.selectCat;
+             object2.CheckCat;
+             object2.DisplayScorecard;
+         };
+             object2.displayFinalScoreboard;
+         };
+
+
+        
+    };
+     int main() {
+         play();
+
+     };
+
+}
+
         };
     };
 
