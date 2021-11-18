0c7a2dd1ed8a657e8c1d62827247171ad1a65403
Adrian Humphrey <humphar@bgsu.edu>
2021-11-17 19:38:02 -0500

    Removed redundant test code

--- Tests/DiceTest.cpp
+++ Tests/DiceTest.cpp
@@ -14,32 +14,20 @@ namespace DiceTest
 	public:
 		
 		// Testing each individual die to ensure they are within the boundaries
-		TEST_METHOD(rngTestOne){
+		TEST_METHOD(rngTest){
 			Dice diceRoll;
 			int val1 = diceRoll.rollDice(diceRoll.dieArray[0]);
 			Assert::AreNotEqual(val1, 0);
 			Assert::AreNotEqual(val1, 7);
-		}
-		TEST_METHOD(rngTestTwo) {
-			Dice diceRoll;
 			int val2 = diceRoll.rollDice(diceRoll.dieArray[1]);
 			Assert::AreNotEqual(val2, 0);
 			Assert::AreNotEqual(val2, 7);
-		}
-		TEST_METHOD(rngTestThree) {
-			Dice diceRoll;
 			int val3 = diceRoll.rollDice(diceRoll.dieArray[2]);
 			Assert::AreNotEqual(val3, 0);
 			Assert::AreNotEqual(val3, 7);
-		}
-		TEST_METHOD(rngTestFour) {
-			Dice diceRoll;
 			int val4 = diceRoll.rollDice(diceRoll.dieArray[3]);
 			Assert::AreNotEqual(val4, 0);
 			Assert::AreNotEqual(val4, 7);
-		}
-		TEST_METHOD(rngTestFive) {
-			Dice diceRoll;
 			int val5 = diceRoll.rollDice(diceRoll.dieArray[4]);
 			Assert::AreNotEqual(val5, 0);
 			Assert::AreNotEqual(val5, 7);
